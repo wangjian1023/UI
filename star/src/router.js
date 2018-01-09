dc27219beecc149ec62ffbf9c405de98ee346bc4
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2018-01-09 21:21:30 +0800

    更新

--- star/src/router.js
+++ star/src/router.js
@@ -3,19 +3,15 @@ import { Router, Route, Switch } from 'dva/router';
 import 'antd/dist/antd.less';
 import BasicLayout from './Layouts/BasicLayout';
 import Write from './routes/write';
-// import view from './routes/view';
-// import book from './routes/book';
 import App from './routes/app';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route exact path="/write/app" component={App} />
+        <Route path="/write/app" component={App} />
         <Route path="/write" render={() => <Write />} />
-        {/* <Route path="/app" render={() => <app />} /> */}
         <Route path="/" component={BasicLayout} />
-        {/* <IndexRoute component={view} /> */}
       </Switch>
     </Router>
   );
