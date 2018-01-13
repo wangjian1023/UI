0ff846688f58b7277f0af96fd18d55736204914e
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2018-01-13 19:28:59 +0800

    组件底部样式

---
+++ star/src/components/footer/footer.js
@@ -0,0 +1,29 @@
+/**
+ * 2018-01-13   Wang Jian
+ */
+
+import React from 'react';
+import styles from './footer.less';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <Footer style={{ textAlign: 'center' }} className={styles.layout}>
+        <span>手机应用</span>
+        <span>|</span>
+        <span>口袋攻略</span>
+        <span>|</span>
+        <span>意见反馈</span>
+        <span>|</span>
+        <span>联系我们</span>
+        <span>|</span>
+        <span style={{ paddingRight: '500px' }}>使用条款</span>
+        <div>
+          <span className={styles.h5}>蝉游记  沪ICP备1号文案</span>
+        </div>
+      </Footer>
+    );
+  }
+}
+
+export default Footer;
